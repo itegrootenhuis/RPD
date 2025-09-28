0df3e018bd6eaba9e5bdbee2c9e218c03511e902
Ian TeGrootenhuis <itegrootenhuis@gmail.com>
2025-09-28 19:04:05 -0400

    Updating godot do 4.5

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -196,8 +196,8 @@ public partial class Game : Node
 
 	private static GameState GetGameState()
 	{
-        var tree = (SceneTree)Engine.GetMainLoop();
-        var gs = (GameState)tree.Root.GetNode( "/root/GameState" );
-        return gs;
+		var tree = (SceneTree)Engine.GetMainLoop();
+		var gs = (GameState)tree.Root.GetNode( "/root/GameState" );
+		return gs;
 	}
 }
